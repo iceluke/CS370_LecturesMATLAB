be1a9d147fa65cecc6d6f5cbb22cd9c2d39c16bc
Luca Giuliani <iceluke@users.noreply.github.com>
2018-09-18 22:07:13 -0400

    some advancement. IEEE binary conversion

--- MyFPNumber.m
+++ MyFPNumber.m
@@ -3,12 +3,50 @@ classdef MyFPNumber
     %   
     
     properties
-        NormalizedForm double
+        Sign uint8
+        Mantissa uint64
+        Exponent uint64
         System MyFPSystem
     end
     
     methods
-
+        function obj = MyFPNumber(real,system)
+            obj.System = system;
+            % %%%%%%%%%
+            % From IEEE 754 double to its binary representation
+            ieee = obj.myNum2bin(real);
+            ieeeSign = bin2dec(ieee(1));
+            % IEEE 754 has exponent bias to avoid sign bit 
+            % = 2^(exponentbits-1)-1 (for double = 1023)
+            % see https://en.wikipedia.org/wiki/Exponent_bias 
+            ieeeExponent = bin2dec(ieee(2:12)) - 1023;
+            % in IEEE754 the mantissa corresponds to 1.b0b1b2...bt
+            ieeeMantissa = bin2dec(ieee(13:64));
+            % %%%%%%%%%
+            % Converting to the lecture's representation
+            % Using uint64 for Exponent allows for incrementing it
+            obj.Sign = ieeeSign; % sign does not change for now as we force positive base
+            [obj.Mantissa,obj.Exponent] = obj.euclideanBaseChange(ieeeMantissa,ieeeExponent,obj.System.Base);
+        end
+    end
+    
+    methods (Access = private)
+        function ieeeBinary = myNum2bin(obj,i) % MATLAB's num2bin is a paid extension
+            % We are using multiple inbuilt conversions because...lazyness
+            hexForm = num2cell(num2hex(i)); % cell array representation
+            hexArray = cellfun(@hex2dec, hexForm, 'UniformOutput',false); % back to dec array
+            binArray = cellfun(@dec2bin, hexArray, 'UniformOutput',false);  % into binary representation
+            % IEEE 754 double precision is 64 bits long, padding if
+            % necessary
+            binArray = cellfun(@obj.binaryBytePadding, binArray, 'UniformOutput',false);
+            ieeeBinary = strcat(cell2mat(binArray));
+        end
+        function b = binaryBytePadding(obj,x) %Makes sure a byte is padded by 4 zeros
+            b = pad(x,4,'left','0');
+        end
+        function euclideanBaseChange(obj,mantissa,exponent,destBase)
+            % TODO
+        end
     end
 end
 
--- MyFPSystem.m
+++ MyFPSystem.m
@@ -18,9 +18,17 @@ classdef MyFPSystem
     methods
         function obj = MyFPSystem(nbDigits,base,lower,upper)
             obj.NbDigits = nbDigits;
+            if ~(base>0) % TODO: implement support for negative bases
+                         % would just have to take care of sign bit
+                disp('Base must be positive')
+                return
+            end
             obj.Base = base;
-            obj.ExponentLower = lower;
-            obj.ExponentUpper = upper;
+            % Exponent bounds are by default casted to integer
+            % TODO: bind them to abs(bound) < abs(IEEE)-5
+            % So that we have room for rounding with 5 digits freedom
+            obj.ExponentLower = uint16(lower);
+            obj.ExponentUpper = uint16(upper);
         end
     end
 end
